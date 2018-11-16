c4570370128bc517c7f903075e6a46377cda2556
itsmysurport <itsmysurport@gmail.com>
2018-11-16 23:33:57 +0900

    파이썬 외주 MYSQL을 이용한 방문록 프로그램 INPUT

---
+++ python/SMTM/guestView_input.py
@@ -0,0 +1,52 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import messagebox
+import pymysql
+import time
+
+conn = pymysql.connect(host='localhost', user='root', password='12345678', db='mydb', charset='utf8')
+curs = conn.cursor()
+sql = "INSERT INTO table1(name, contents, time) VALUES (%s, %s, %s)"
+
+def onClick():
+    global conn, curs, sql
+    name = entryName.get()
+    contents = entryComp.get("1.0", END)
+    tm = time.strftime('%H:%M:%S')
+    curs.execute(sql, (name, contents, tm))
+    conn.commit()
+    txt = "Your content was saved!"
+    messagebox.showinfo("My SQL DB", txt)
+
+root = Tk()
+root.geometry("600x400+100+100")
+
+
+root.title("Questbook using MySQL")
+
+frame1 = Frame(root)
+frame1.pack(fill=X)
+
+lblName = Label(frame1, text="Name : ", width=10)
+lblName.pack(side=LEFT, padx=10, pady=10)
+
+entryName = Entry(frame1)
+entryName.pack(fill=X, padx=10, expand=True)
+
+# 내용
+frame2 = Frame(root)
+frame2.pack(fill=X)
+
+lblComp = Label(frame2, text="Connect :", width=10)
+lblComp.pack(side=LEFT, padx=10, pady=10)
+
+entryComp = Text(frame2)
+entryComp.pack(fill=X, padx=10, expand=True)
+
+# 저장
+frame4 = Frame(root)
+frame4.pack(fill=X)
+btnSave = Button(frame4, text="Submit", width = 10, command=onClick)
+btnSave.pack(side=LEFT, padx=100, pady=10)
+
+root.mainloop()
